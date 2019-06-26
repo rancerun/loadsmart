ece087cbd3b736d7dd796d6778f3dd529fe602cd
rancerun <lancesun22@gmail.com>
2019-06-26 16:37:24 -0400

    refactored code to change data structure & tests

--- app/primary_secondary_options.py
+++ app/primary_secondary_options.py
@@ -3,28 +3,27 @@
 import calculations as cal
 
 def primary_secondary_options(cargos, trucks):
-  first_option = {} 
-  second_option = {}
+  cargo_options = {}
 
   for cargo_name, cargo_info in cargos.items():
-    first_option[cargo_name] = {'null': float('inf')}
-    second_option[cargo_name] = {'null': float('inf')}
+    cargo_options[cargo_name] = [{'null': float('inf')}, {'null': float('inf')}]
 
     for truck_name, truck_info in trucks.items():
-      first_option_truck_distance = first_option[cargo_name].values()[0]
-      second_option_truck_distance = second_option[cargo_name].values()[0]
+      first_option_distance = cargo_options[cargo_name][0].values()[0]
+      second_option_distance = cargo_options[cargo_name][1].values()[0]
 
       truck_to_origin_distance = cal.calculate_distance(truck_info['lng'], truck_info['lat'], cargo_info['o_lng'], cargo_info['o_lat'])
       truck_to_end_distance = cal.calculate_distance(truck_info['lng'], truck_info['lat'], cargo_info['d_lng'], cargo_info['d_lat'])
       current_truck_distance = truck_to_origin_distance + truck_to_end_distance
 
-      if first_option_truck_distance > current_truck_distance:
-        second_option[cargo_name] = first_option[cargo_name]
-        first_option[cargo_name] = {
+      if first_option_distance > current_truck_distance:
+        cargo_options[cargo_name][1] = cargo_options[cargo_name][0]
+        cargo_options[cargo_name][0] = {
           truck_name: current_truck_distance
         }
-      elif second_option_truck_distance > current_truck_distance:
-        second_option[cargo_name] = {
+      elif second_option_distance > current_truck_distance:
+        cargo_options[cargo_name][1] = {
           truck_name: current_truck_distance
         }
-  return first_option, second_option
+
+  return cargo_options
